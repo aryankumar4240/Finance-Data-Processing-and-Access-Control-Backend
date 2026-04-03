aca1b6a6cf1560af8011ab348a70a2936c7d6523
Aryan Kumar <aryankumar424046@gmail.com>
2026-04-03 11:56:37 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    req.user = decoded; // { id, role }
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
